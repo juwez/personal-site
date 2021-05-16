b610775ba9a022e2a68d7c20a8922a307c3d29c1
juwez <joeylybaert@outlook.com>
2021-05-16 12:47:02 +0200

    building this blog post

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <nav>
-      <h1>Next'Blog</h1>
+      <h1>Joey Lybaert's Blog</h1>
       <div>
         <Link href="/">
           <a>Blog</a>
-        </Link>
-        <Link href="/">
-          <a>About</a>
         </Link>
       </div>
       <style jsx>{`
